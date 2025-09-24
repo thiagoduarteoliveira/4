321e813fbb76ab047057d7c693ca0fea1285dcea
Thiago Duarte <137238345+thiagoduarteoliveira@users.noreply.github.com>
2025-09-24 15:51:27 -0300

    Update logout.php

--- public/admin/logout.php
+++ public/admin/logout.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 require __DIR__ . '/../../vendor/autoload.php';
 use App\Auth;
+
 Auth::logout();
-header('Location: /admin/login');
+header('Location: /admin/login.php');
 exit;
